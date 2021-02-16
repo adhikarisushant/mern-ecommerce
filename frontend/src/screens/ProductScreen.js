d55323135fb6c3855b6d905a2ad2b2add270d2ee
Sushant <sushant.adhikari.619@gmail.com>
2021-02-16 10:38:06 +0545

    cart screen and remove items from cart

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -77,7 +77,7 @@ const ProductScreen = ({ history, match }) => {
                                         <Form.Control as="select" value={qty} onChange={(e) => setQty(e.target.value)}>
                                             {
                                             [...Array(product.countInStock).keys()].map(x =>(
-                                                <option key={x + 1} value={x + 1}>
+                                                <option key={x+1} value={x + 1}>
                                                     {x + 1}
                                                 </option>
                                             ))
