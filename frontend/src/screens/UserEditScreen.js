911e3600b7ecb51ddf12ede0e58918a23023620f
Sushant <sushant.adhikari.619@gmail.com>
2021-05-24 20:23:26 +0545

    update user functionality

--- frontend/src/screens/UserEditScreen.js
+++ frontend/src/screens/UserEditScreen.js
@@ -5,7 +5,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
-import { getUserDetails } from '../actions/userActions'
+import { getUserDetails, updateUser } from '../actions/userActions'
+import { USER_UPDATE_RESET } from '../constants/userConstants'
 
 const UserEditScreen = ({ match, history }) => {
     const userId = match.params.id
@@ -19,19 +20,27 @@ const UserEditScreen = ({ match, history }) => {
     const userDetails = useSelector(state => state.userDetails)
     const { loading, error, user } = userDetails
 
+    const userUpdate = useSelector(state => state.userUpdate)
+    const { loading: loadingUpdate, error: errorUpdate, success: successUpdate, } = userUpdate
 
     useEffect(() => {
-        if(!user.name || user._id !== userId) {
-            dispatch(getUserDetails(userId))
-        } else {
-            setName(user.name)
-            setEmail(user.email)
-            setIsAdmin(user.isAdmin)
-        }
-    }, [dispatch, userId, user])
+            if(successUpdate) {
+                dispatch({ type: USER_UPDATE_RESET })
+                history.push('/admin/userlist')
+            } else {
+                if(!user.name || user._id !== userId) {
+                            dispatch(getUserDetails(userId))
+                        } else {
+                            setName(user.name)
+                            setEmail(user.email)
+                            setIsAdmin(user.isAdmin)
+                        }
+            }
+    }, [dispatch, history, userId, user, successUpdate])
 
     const submitHandler = (e) => {
         e.preventDefault()
+        dispatch(updateUser({ _id: userId, name, email, isAdmin }))
     }
 
     return (
@@ -42,6 +51,8 @@ const UserEditScreen = ({ match, history }) => {
 
             <FormContainer>
             <h1>Edit User</h1>
+            {loadingUpdate && <Loader />}
+            {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
             {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
             <Form onSubmit={submitHandler}>
                 <Form.Group controlId='name'>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,7 +6,7 @@ import {
   productDetailsReducer,
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
-import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer, userListReducer, userDeleteReducer } from './reducers/userReducers'
+import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer, userListReducer, userDeleteReducer, userUpdateReducer } from './reducers/userReducers'
 import { orderCreateReducer, orderDetailsReducer, orderPayReducer, orderListMyReducer } from './reducers/orderReducers'
 
 
@@ -20,6 +20,7 @@ const reducer = combineReducers({
   userUpdateProfile: userUpdateProfileReducer,
   userList: userListReducer,
   userDelete: userDeleteReducer,
+  userUpdate: userUpdateReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
