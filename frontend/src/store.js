3ecc38e4a4224155a5c2352ebc8c3c1febf53579
Sushant <sushant.adhikari.619@gmail.com>
2021-05-27 22:18:38 +0545

    Admin Order List

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -10,7 +10,7 @@ import {
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer, userListReducer, userDeleteReducer, userUpdateReducer } from './reducers/userReducers'
-import { orderCreateReducer, orderDetailsReducer, orderPayReducer, orderListMyReducer } from './reducers/orderReducers'
+import { orderCreateReducer, orderDetailsReducer, orderPayReducer, orderListMyReducer, orderListReducer, } from './reducers/orderReducers'
 
 
 const reducer = combineReducers({
@@ -31,6 +31,7 @@ const reducer = combineReducers({
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
   orderListMy: orderListMyReducer,
+  orderList: orderListReducer,
 
 })
 
