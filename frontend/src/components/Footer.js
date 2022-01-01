b275be35ea60543a861f16a8a75c77c09ec577ed
adhikarisushant <sushant.adhikari.619@gmail.com>
2022-01-01 20:43:44 +0545

    ready for deployment

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer>
             <Container>
                 <Row>
-                    <Col className='text-center py-3'>Copyright &copy; E-Shop</Col>
+                    <Col className='text-center py-3'>Copyright &copy; E-Shop Sushant Adhikari</Col>
                 </Row>
             </Container>
         </footer>
